19cad53f7e1b546ae49e06e5b5aa03cabd9325e4
todd_wu <todd404@outlook.com>
2023-04-26 10:10:27 +0800

    🔧子评论动态化

--- src/components/problem/Comment/SubComment.js
+++ src/components/problem/Comment/SubComment.js
@@ -3,17 +3,8 @@ import { UpOutlined } from '@ant-design/icons'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const data = Array.from({ length: 23 }).map((_, i) => ({
-    id: i,
-    href: 'https://ant.design',
-    title: `ant design part ${i}`,
-    avatar: `https://xsgames.co/randomusers/avatar.php?g=pixel&key=${i}`,
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-}));
-
 const getData = async (commnet_id)=>{
-    let res = await axios.get(`http://192.168.50.168:10393/mock/9e9ed3f6-20a8-4c4f-8fa7-6181902f7308/api/sub_comment?apipost_id=d6046a`)
+    let res = await axios.get(`http://localhost:10393/mock/9e9ed3f6-20a8-4c4f-8fa7-6181902f7308/api/sub_comment?apipost_id=d6046a`)
     return res.data.data;
 }
 
@@ -60,9 +51,9 @@ export default function SubCommnet({commnetId, onFoldCLick}){
                         key={item.id}
                     >
                         <List.Item.Meta
-                            avatar={<Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />}
-                            title={<a href="https://ant.design">{item.title}</a>}
-                            description={"这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n这是一个子回复\n"}
+                            avatar={<Avatar src={item.user_avatar} />}
+                            title={item.user_name} //TODO:改成用户链接
+                            description={item.content}
                         />
                     </List.Item>
                 )}
