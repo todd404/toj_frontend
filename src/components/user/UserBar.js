4d12fbaaffbe80c7f32537184ae7cd9965ba588a
todd_wu <todd404@outlook.com>
2023-05-17 16:27:47 +0800

    🔨fixed:点击已登录的头像不跳转到用户中心的问题

--- src/components/user/UserBar.js
+++ src/components/user/UserBar.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 import UserContent from "./UserContent"
 import MessageList from "./MessageList"
+import {useNavigate} from "umi"
 
 async function getUserInfo(){
 	let url = `http://localhost:10393/mock/9e9ed3f6-20a8-4c4f-8fa7-6181902f7308/api/userinfo`
@@ -19,6 +20,8 @@ async function getMessages(){
 }
 
 export default function UserBar(){
+	const navigate = useNavigate();
+
     const [loginOpen, setLoginOpen] = useState(false);
 	const [infoOpen, setInfoOpen] = useState(false);
 	const [userInfo, setUserInfo] = useState({
@@ -37,6 +40,7 @@ export default function UserBar(){
 	})
 
 	const openLogin = ()=>setLoginOpen(true);
+	const openUserCenter = ()=>navigate("/usercenter")
 
 	const updateUserInfo = async ()=>{
 		let userInfo = await getUserInfo();
@@ -70,7 +74,7 @@ export default function UserBar(){
 				onOpenChange={(newOpen)=>{setInfoOpen(newOpen && userInfo.success)}}
 				content={(<UserContent username={userInfo.userinfo.user_name}/>)}
 			>
-				<Avatar src={userInfo.userinfo.avatar} onClick={userInfo.success ? ()=>{} : openLogin} icon={<UserOutlined/>} style={{ cursor: "pointer" }}/>
+				<Avatar src={userInfo.userinfo.avatar} onClick={userInfo.success ? openUserCenter : openLogin} icon={<UserOutlined/>} style={{ cursor: "pointer" }}/>
 			</Popover>
             <Login open={loginOpen} onOk={()=>setLoginOpen(false)} onCancel={()=>setLoginOpen(false)}/>
           </Space>
